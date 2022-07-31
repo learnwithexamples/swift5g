839517612e1e99b876bc4b5411e3fb6df56fa46f
Learn with Examples <learnwithexamples@outlook.com>
2022-07-31 08:51:13 -0700

    update

--- Tests/swift5gTests/swift5gTests.swift
+++ Tests/swift5gTests/swift5gTests.swift
@@ -2,23 +2,4 @@ import XCTest
 @testable import swift5g
 
 final class swift5gTests: XCTestCase {
-  func testTs38101_1() throws {
-    XCTAssertEqual(Ts38101_1.Table5p1_1.fHz2Fr(50), nil)
-    XCTAssertEqual(Ts38101_1.Table5p1_1.fHz2Fr(500), 1)
-    XCTAssertEqual(Ts38101_1.Table5p1_1.fHz2Fr(50000), 2)
-    XCTAssertEqual(Ts38101_1.Table5p1_1.frequencyRangeDesignation["FR1"]!.lowerBound, 410)
-    XCTAssertEqual(Ts38101_1.Table5p1_1.frequencyRangeDesignation["FR1"]!.upperBound, 7125)
-    XCTAssertEqual(Ts38101_1.Table5p1_1.frequencyRangeDesignation["FR2"]!.lowerBound, 24250)
-    XCTAssertEqual(Ts38101_1.Table5p1_1.frequencyRangeDesignation["FR2"]!.upperBound, 52600)
-    XCTAssertEqual(Ts38101_1.Table5p2_1.nrOperatingBand[3]!.1!.lowerBound, 1805)
-    XCTAssertEqual(Ts38101_1.Table5p2_1.nrOperatingBand[48]!.0!.upperBound, 3700)
-    XCTAssertEqual(Ts38101_1.Table5p2_1.lookup(band: 48)!.0!.upperBound, 3700)
-    XCTAssertEqual(Ts38101_1.Table5p2_1.lookup(freq: 1890)[2]!.duplex, Types.DuplexMode.FDD)
-    XCTAssertEqual(Ts38101_1.Table5p2Ap1_1.nrCaBand(1), "CA_n1")
-    XCTAssertEqual(Ts38101_1.Table5p2Ap1_1.nrCaBand(2), nil)
-    XCTAssertEqual(Ts38101_1.Table5p2Ap1_2.nrCaBand(3), "CA_n3(*)")
-    XCTAssertEqual(Ts38101_1.Table5p2Ap2p1_1.nrCaBand(78, 1), "CA_n1-n78")
-    XCTAssertEqual(Ts38101_1.Table5p2Ap2p1_1.nrCaBand(2, 1), nil)
-//    print(Ts38101_1.Table5p2_1.lookup(freq: 1890).sorted(by: {$0.0 < $1.0}))
-  }
 }
